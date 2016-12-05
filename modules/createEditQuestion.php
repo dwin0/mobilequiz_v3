1f99f1c5d28bab74a4a1320ba7f2f7735294e0b0
dwin94 <dwindler@hsr.ch>
2016-12-05 21:29:25 +0100

    CreateEditQuestion: removed unused html

--- modules/createEditQuestion.php
+++ modules/createEditQuestion.php
@@ -381,6 +381,7 @@
 			</div>
 			
 			
+			<!-- Last Edited -->
 			<?php if($mode == 'edit') {?>
 			<div class="form-group">
 				<label class="col-md-2 col-sm-3 control-label"> 
@@ -433,7 +434,8 @@
 				
 		</div>
 		
-				
+		
+		<!-- Answers -->
 		<div class="tab-pane fade form-horizontal" id="answers">
 		
 			<div class="form-group">
@@ -526,357 +528,27 @@
 					<?php }?>
 				</div>
 			</div>
-			
 		</div>
 	</div>
 	
 	<div>
-			<div style="text-align: left; float: left;">
-				<input type="button" class="btn" id="btnBack"
-					value="<?php echo $lang["btnBack"];?>"/>
-			</div>
+		<div style="text-align: left; float: left;">
+			<input type="button" class="btn" id="btnBack"
+				value="<?php echo $lang["btnBack"];?>"/>
+		</div>
 
-			<div style="text-align: right; margin-top: 10px;">
-				<input type="hidden" name="mode" value="<?php echo $mode;?>">
-				<input type="hidden" name="question_id" value="<?php echo ($mode == "edit") ? $questionFetch["id"] : $newQuestionId;?>">
-				<input type="hidden" name="fromsite" value="<?php echo isset($_GET["fromsite"]) ? $_GET["fromsite"] : '';?>">
-				<input type="hidden" name="fromQuizId" value="<?php echo isset($_GET["quizId"]) ? $_GET["quizId"] : '';?>">
-				<input type="button" class="btn" id="btnSaveAndNext"
-					value="<?php echo $lang["createNext"];?>" />
-			</div>
+		<div style="text-align: right; margin-top: 10px;">
+			<input type="hidden" name="mode" value="<?php echo $mode;?>">
+			<input type="hidden" name="question_id" value="<?php echo ($mode == "edit") ? $questionFetch["id"] : $newQuestionId;?>">
+			<input type="hidden" name="fromsite" value="<?php echo isset($_GET["fromsite"]) ? $_GET["fromsite"] : '';?>">
+			<input type="hidden" name="fromQuizId" value="<?php echo isset($_GET["quizId"]) ? $_GET["quizId"] : '';?>">
+			<input type="button" class="btn" id="btnSaveAndNext"
+				value="<?php echo $lang["createNext"];?>" />
 		</div>
-	
-	
+	</div>
 
+	<p id="failMsg" style="color: red; margin-top: 15px"></p>
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	<form id="createQuestion" action="<?php echo "?p=actionHandler&action=insertQuestion&mode=" . $mode;?>" class="form-horizontal" method="POST" enctype="multipart/form-data" onsubmit="return formCheck()">
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h3 class="panel-title"><?php echo $lang["generalInformations"]?></h3>
-			</div>
-			<div class="panel-body">
-				<div class="form-group">
-					<label for="questionText" class="col-md-2 col-sm-3 control-label">
-						<?php echo $lang["questionQuestionTextOnly"];?> *
-					</label>
-					<div class="col-md-10 col-sm-9">
-						<textarea name="questionText" wrap="soft" id="questionText"
-							class="form-control text-input" required="required"
-							placeholder="<?php echo $lang["questionQuestionTextOnly"] . " (" . $lang["maximum"] . " " . MAX_CHARACTERS_PER_QUESTION . " " . $lang["characters"] . ")";?>" maxlength="<?php echo MAX_CHARACTERS_PER_QUESTION;?>"><?php 
-							if($mode == "edit")
-							{
-								echo $questionFetch["text"];
-							}
-							?></textarea>
-							<p id="questionTextHelp" title="Wie m&uuml;ssen Texte eingegeben werden?" style="cursor: pointer; text-decoration: underline;"><?php echo $lang["help"];?></p>
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="keywords" class="col-md-2 col-sm-3 control-label">
-						<?php echo $lang["navHeaderKeywords"];?>
-					</label>
-					<div class="col-md-10 col-sm-9">
-					<?php 
-					if($mode == "edit" && isset($_GET["id"]))
-					{
-						$stmt = $dbh->prepare("select keyword.word from qu_keyword inner join keyword on keyword.id = qu_keyword.keyword_id where qu_keyword.qu_id = :qu_id");
-						$stmt->bindParam(":qu_id", $_GET["id"]);
-						$stmt->execute();
-						$keywordFetch = $stmt->fetchAll(PDO::FETCH_ASSOC);
-					}
-					?>
-						<input type="text" name="keywords" id="keywords" class="form-control"
-							value="<?php 
-							if($mode == "edit")
-							{
-								for($i = 0; $i < count($keywordFetch); $i++)
-								{
-									if($i > 0)
-										echo ",";
-									echo $keywordFetch[$i]["word"];
-								}
-							}
-							?>" data-role="tagsinput" />
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="questionLogo" class="col-md-2 col-sm-3 control-label">
-						<?php echo $lang["picture"];?>
-					</label>
-					<div class="col-md-10 col-sm-9" id="questionLogoWrapper">
-						<input type="file" id="questionLogo" name="questionLogo"
-							class="btn" accept=".jpeg,.jpg,.bmp,.png,.gif"/>
-						<div id="picturePreview">
-						<?php if($mode == "edit" && $questionFetch["picture_link"] != "")
-						{
-							echo "<br /><img style=\"float:left; max-width:300px; max-height:300px\" src=\"" . $questionFetch["picture_link"] . "\" width=\"50%\" ></img>";
-							?>
-							<img style="margin-left: 10px;" src="assets/icon_delete.png" alt="" title="" height="18px" width="18px" onclick="delPicture(<?php echo $questionFetch["id"];?>)">
-							<?php 
-						}?>	
-						</div>
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="language" class="col-md-2 col-sm-3 control-label"> 
-						<?php echo $lang["quizLanguage"];?> *
-					</label>
-					<div class="col-md-10 col-sm-9">
-						<select id="language" class="form-control" name="language">
-		                	<?php 
-		                	$stmt = $dbh->prepare("select id from question");
-		                	$stmt->execute();
-		                	$allQuestionsCount = $stmt->rowCount();
-		                	
-		                    $stmt = $dbh->prepare("select language from question group by language");
-		                    $stmt->execute();
-		                    $result = $stmt->fetchAll();
-		                    
-		                    for($i = 0; $i < count($result); $i++){
-								$stmt = $dbh->prepare("select id from question where language = '" . $result[$i]["language"] . "'");
-								$stmt->execute();
-								$selected = "";
-								if($mode == "edit")
-									$selected = ($questionFetch["language"] == $result[$i]["language"]) ? 'selected="selected"' : '';
-								echo "<option value=\"" . $result[$i]["language"] . "\"" . $selected . ">" . $result[$i]["language"] . " (" . $stmt->rowCount() . " " . $lang["quizzes"] . ")</option>";
-		                    } ?>
-		                </select>
-		                <input type="text" id="newLanguage"
-							class="form-control" name="newLanguage"
-							placeholder="<?php echo $lang["newLanguagePlaceholder"];?>" maxlength="30"/>
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="applicationArea" class="col-md-2 col-sm-3 control-label"> 
-						<?php echo $lang["quizTableTopic"];?> *
-					</label>
-					<div class="col-md-10 col-sm-9">
-						<select id="topic" class="form-control" name="topic">
-			                    <?php 
-			                    $stmt = $dbh->prepare("select * from subjects");
-			                    $stmt->execute();
-			                    $result = $stmt->fetchAll();
-			                    
-			                    for($i = 0; $i < count($result); $i++){
-									$stmt = $dbh->prepare("select id from question where subject_id = " . $result[$i]["id"]);
-									$stmt->execute();
-									$rowCount = $stmt->rowCount();
-									
-									$selected = "";
-									if($mode == "edit")
-										$selected = ($questionFetch["subject_id"] == $result[$i]["id"]) ? 'selected="selected"' : '';
-									echo "<option value=\"" . $result[$i]["id"] . "\" " . $selected . ">" . $result[$i]["name"] . " (" . $rowCount . " " . $lang["quizzes"] . ")</option>";
-			                    } ?>
-			                    <option value="null" <?php if($mode != "edit") {echo ' selected="selected"';} else {
-			                    	if($questionFetch["subject_id"] == NULL) {echo ' selected="selected"';}
-			                    }?>>Nicht zugeordnet <?php 
-				                    $stmt = $dbh->prepare("select id from question where subject_id is null");
-				                    $stmt->execute();
-				                    echo " (" . $stmt->rowCount() . " " . $lang["quizzes"] . ")";
-			                    ?></option>
-			                </select>
-						<input type="text" id="newTopic"
-							class="form-control" name="newTopic"
-							placeholder="<?php echo $lang["newTopicPlaceholder"] . " (" . $lang["maximum"] . " " . MAX_CHARACTERS_PER_TOPIC . " " . $lang["characters"] . ")";?>" maxlength="<?php echo MAX_CHARACTERS_PER_TOPIC?>"/>
-					</div>
-				</div>
-				<?php if($mode == 'edit') {?>
-				<div class="form-group">
-					<label class="col-md-2 col-sm-3 control-label"> 
-			        	<?php echo $lang["createdBy"];?>
-					</label>
-					<div class="col-md-10 col-sm-9">
-						<p class="form-control-static">
-						<?php 
-							if($mode == "edit")
-							{
-								echo $questionFetch["firstname"] . " " . $questionFetch["lastname"];
-							}
-						?>
-						</p>
-					</div>
-				</div>
-				<div class="form-group">
-					<label class="col-md-2 col-sm-3 control-label"> 
-			        	<?php echo $lang["createdAt"];?>
-					</label>
-					<div class="col-md-10 col-sm-9">
-						<p class="form-control-static">
-						<?php 
-							if($mode == "edit")
-							{
-								echo date("d.m.Y H:i:s", $questionFetch["creation_date"]);
-							}
-						?>
-						</p>
-					</div>
-				</div>
-				<div class="form-group">
-					<label class="col-md-2 col-sm-3 control-label"> 
-			        	<?php echo $lang["lastModified"];?>
-					</label>
-					<div class="col-md-10 col-sm-9">
-						<p class="form-control-static">
-						<?php 
-							if($mode == "edit")
-							{
-								echo date("d.m.Y H:i:s", $questionFetch["last_modified"]);
-							}
-						?>
-						</p>
-					</div>
-				</div>
-				<?php }?>
-			</div>
-		</div>
-		<div class="panel panel-default">
-			<div class="panel-body">
-				<div class="row">
-					<div class="col-md-2 col-sm-3 col-xs-3"></div>
-					<div class="col-md-9 col-sm-7 col-xs-7"></div>
-					<div class="col-md-1 col-sm-2 col-xs-2">
-						<label id="correctAnswerHeading"> 
-			            	<?php echo $lang["correctAnswer"];?>
-						</label>
-					</div>
-				</div>
-				<?php 
-				if($mode == "edit")
-				{
-					$stmt = $dbh->prepare("select question.id as Qid, answer_question.*, answer.text from question inner join answer_question on answer_question.question_id = question.id inner join answer on answer_question.answer_id = answer.id where question.id = :id order by `order` asc;");
-					$stmt->bindParam(":id", $_GET["id"]);
-					$stmt->execute();
-					$answerFetch = $stmt->fetchAll(PDO::FETCH_ASSOC);
-				}
-				for($i = 0; $i < 5; $i++) {
-				?>
-				<div class="row">
-					<div class="col-md-2 col-sm-3 col-xs-3 control-label">
-						<label id="<?php echo "answerText_" . $i;?>"> 
-			            	<?php echo $lang["answertext"] . " " . ($i+1);
-			            	if($i < 2)
-			            		echo " *";
-			            	?>
-						</label>
-					</div>
-					<div class="col-md-9 col-sm-7 col-xs-7">
-						<textarea name="<?php echo "answerText_" . $i;?>" wrap="soft"
-							class="form-control" <?php echo $i < 2 ? " required='required' " : ''?>
-							placeholder="<?php echo $lang["answertext"] . " " . ($i+1) . " (" . $lang["maximum"] . " " . MAX_CHARACTERS_PER_ANSWER . " " . $lang["characters"] . ")";?>" maxlength="<?php echo MAX_CHARACTERS_PER_ANSWER;?>"><?php 
-							if($mode == "edit")
-							{
-								if($i < $stmt->rowCount())
-									echo $answerFetch[$i]["text"];
-							}
-							?></textarea>
-					</div>
-					<div class="col-md-1 col-sm-2 col-xs-2">
-						<input id="<?php echo "correctAnswer_" . $i;?>" type="checkbox" name="<?php echo "correctAnswer_" . $i;?>"
-							class="checkbox" value="1" 
-							<?php if($i < $stmt->rowCount()) 
-							{
-								if($answerFetch[$i]["is_correct"] == 1)
-									echo 'checked="checked"';
-							}
-							?> 
-						/>
-					</div>
-				</div>
-				<?php }?>
-			</div>
-		</div>
-		<div class="panel panel-default">
-			<div class="panel-body">
-				<div class="row">
-					<div class="col-md-2 col-sm-3 control-label">
-						<label> 
-			            	<?php echo $lang["showAs"];?> *
-						</label>
-					</div>
-					<div class="col-md-10 col-sm-9 radio-inline">
-						<label class="radio-inline"> <input type="radio"
-							name="questionType" id="questionTypeSingleChoice"
-							value="singelchoise" required
-							<?php if($mode == "create") { echo "checked"; }
-							else if($mode == "edit") {
-								if($questionFetch["type_id"] == 1)
-									echo " checked";
-							}
-							?> /> <?php echo $lang["singleChoise"];?>
-						</label> 
-						<label class="radio-inline"> <input type="radio"
-							name="questionType" id="questionTypeMultipleChoice"
-							value="multiplechoise" <?php if($mode == "edit") {
-								if($questionFetch["type_id"] == 2)
-									echo " checked";
-							}?>/>
-							<?php echo $lang["multipleChoise"];?>
-						</label>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-2 col-sm-3 control-label">
-						<label> 
-							<?php echo $lang["publication"];?> *
-						</label>
-					</div>
-					<div class="col-md-10 col-sm-9 radio-inline">
-						<label class="radio-inline"> <input type="radio" name="isPrivate"
-							value="0" required 
-							<?php if($mode == "create") { echo "checked"; }
-							else if($mode == "edit") {
-								if($questionFetch["public"] == 0)
-									echo " checked";
-							}?>/> <?php echo $lang["public"];?>
-						</label> 
-						<label class="radio-inline"> <input type="radio"
-							name="isPrivate" value="1"
-							<?php if($mode == "edit") {
-								if($questionFetch["public"] == 1)
-									echo " checked";
-							}?>/> <?php echo $lang["privateMoreInfo"];?>
-						</label>
-					</div>
-				</div>
-			</div>
-		</div>
-		<p>
-			<?php echo $lang["requiredFields"];?>
-		</p>
-		<p id="failMsg" style="color:red;"></p>
-		<div>
-			<div style="text-align: left; float: left;">
-				<input type="button" class="btn" id="btnCancel"
-					value="<?php echo $lang["buttonCancel"];?>" onclick="window.location='?p=questions';"/>
-			</div>
-
-			<div style="text-align: right">
-				<input type="hidden" name="mode" value="<?php echo $mode;?>">
-				<input type="hidden" name="question_id" value="<?php echo ($mode == "edit") ? $questionFetch["id"] : $newQuestionId;?>">
-				<input type="hidden" name="fromsite" value="<?php echo isset($_GET["fromsite"]) ? $_GET["fromsite"] : '';?>">
-				<input type="hidden" name="fromQuizId" value="<?php echo isset($_GET["quizId"]) ? $_GET["quizId"] : '';?>">
-				<input type="submit" class="btn" name="btnSave" form="createQuestion"
-					value="<?php echo $lang["saveQuestion"];?>" /> 
-				<input type="submit" class="btn" name="btnSaveAndNext" form="createQuestion"
-					value="<?php echo $lang["saveQuestion"] . " " . $lang["createNext"];?>" />
-			</div>
-		</div>
-	</form>
 </div>
 
 
